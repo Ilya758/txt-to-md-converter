108734e067eabcfd0ed315db875c4a3d8a3bd2c6
Ilya758 <subscriber6436@gmail.com>
2022-05-04 14:51:04 +0300

    feat: implemented basic layout of component App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { FormEvent, useReducer } from 'react';
+import { setTextareaValue } from '../store/actions';
+import { AppContext } from '../global/context/AppContext';
+import { initialState } from '../store/initialState';
+import { reducer } from '../store/reducer';
 import './App.styles.scss';
+import Boards from './core/Boards/Boards';
+import ButtonsPanel from './core/ButtonsPanel/ButtonsPanel';
 
 const App = () => {
-  return <div>App</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleTextAreaInput = (e: FormEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target as HTMLTextAreaElement;
+    dispatch(setTextareaValue(value));
+  };
+
+  const context = { state, dispatch, handleTextAreaInput };
+
+  return (
+    <>
+      <AppContext.Provider value={context}>
+        <ButtonsPanel />
+        <Boards />
+      </AppContext.Provider>
+    </>
+  );
 };
 
 export default App;
